ec001d7036250f7e35911baa72337c6fd6704df8
Daniel <kr.danielreis@gmail.com>
2022-01-28 18:06:49 -0300

    @DanielSantosReis [Modificando duas models e um controller do Blog Pessoal]

--- src/main/java/org/generation/blogPessoal/models/Postagem.java
+++ src/main/java/org/generation/blogPessoal/models/Postagem.java
@@ -21,15 +21,14 @@ public class Postagem {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-
 	private long id;
+	
 	@NotNull
-	@Size(min = 5, max = 100)
-
+	@Size(max = 100)
 	private String titulo;
+	
 	@NotNull
 	@Size(min = 10, max = 500)
-
 	private String texto;
 
 	@Temporal(TemporalType.TIMESTAMP)
