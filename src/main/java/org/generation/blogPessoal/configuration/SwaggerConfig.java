7a5d9f098d36f08f2add663ac0ea3c3871160f84
Daniel <kr.danielreis@gmail.com>
2022-02-22 11:52:09 -0300

    atualizado

--- src/main/java/org/generation/blogPessoal/configuration/SwaggerConfig.java
+++ src/main/java/org/generation/blogPessoal/configuration/SwaggerConfig.java
@@ -2,6 +2,8 @@ package org.generation.blogPessoal.configuration;
 
 import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -10,6 +12,7 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 
+@Configuration
 public class SwaggerConfig {
 
 	@Bean
